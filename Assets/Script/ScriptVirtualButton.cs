81fc5b2f6a35d299c009e03b9ce5b95ef434762d
ZukhrufMifta <zukhruf.m.f@gmail.com>
2018-07-23 16:34:13 +0700

    Initial commit

---
+++ Assets/Script/ScriptVirtualButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ScriptVirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vbObject;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Ditekan!!!");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Dilepas!!!");
+    }
+
+    // Use this for initialization
+    void Start () {
+        vbObject = GameObject.Find("BtnVirtual");
+        vbObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+}
